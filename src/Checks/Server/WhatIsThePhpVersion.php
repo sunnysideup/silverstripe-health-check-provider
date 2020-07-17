d57d0c58302634354db280793ee648e17938505a
Nicolaas Thiemen Francken <github@sunnysideup.co.nz>
2020-07-17 13:16:34 +1200

    MINOR: adding PHP version

---
+++ src/Checks/Server/WhatIsThePhpVersion.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Sunnysideup\HealthCheckProvider\Checks\Server;
+
+use Sunnysideup\HealthCheckProvider\Checks\HealthCheckItemRunner;
+
+class WhatIsThePhpVersion extends HealthCheckItemRunner
+{
+    #######################
+    ### Names Section
+    #######################
+
+    public function getCalculatedAnswer()
+    {
+        return phpversion();
+    }
+}
