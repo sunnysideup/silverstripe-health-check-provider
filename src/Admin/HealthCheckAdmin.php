8afa84e9a96cbfb526950345e8312d24dca22a12
Nicolaas Thiemen Francken <github@sunnysideup.co.nz>
2020-07-18 13:12:39 +1200

    PATCH: better spelling

--- src/Admin/HealthCheckAdmin.php
+++ src/Admin/HealthCheckAdmin.php
@@ -15,7 +15,7 @@ class HealthCheckAdmin extends ModelAdmin
 
     private static $url_segment = 'health';
 
-    private static $menu_title = 'health check';
+    private static $menu_title = 'Health Check';
 
     private static $menu_icon_class = 'font-icon-checklist';
 }
