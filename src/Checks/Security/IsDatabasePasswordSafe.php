47bc43a94057e5e85c674ce86415559584455337
Nicolaas Thiemen <nt@sunnysideup.co.nz>
2020-07-30 10:38:23 +1200

    MINOR: adding more checks

---
+++ src/Checks/Security/IsDatabasePasswordSafe.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Sunnysideup\HealthCheckProvider\Checks\Security;
+
+use SilverStripe\Security\Group;
+use Sunnysideup\HealthCheckProvider\Checks\HealthCheckItemRunner;
+
+class IsDatabasePasswordSafe extends HealthCheckItemRunner
+{
+    public function getCalculatedAnswer(): string
+    {
+        return $this->checkPassword(Environment::getEnv('SS_DATABASE_PASSWORD'));
+    }
+}
---
+++ src/Checks/Security/IsDefaultAdminPasswordSafe.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Sunnysideup\HealthCheckProvider\Checks\Security;
+
+use SilverStripe\Security\Group;
+use Sunnysideup\HealthCheckProvider\Checks\HealthCheckItemRunner;
+
+class IsDefaultAdminPasswordSafe extends HealthCheckItemRunner
+{
+    public function getCalculatedAnswer(): string
+    {
+        return $this->checkPassword(Environment::getEnv('SS_DEFAULT_ADMIN_USERNAME'));
+    }
+}
